f57af8ead660626be1c3918c2c221770f411899e
Alexandre <arnaldinhoborgo@gmail.com>
2019-11-07 08:40:11 -0200

    Página de SIGNUP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SignupPage } from './signup/signup.page';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'signup', component: SignupPage },
 ];
 
 @NgModule({
